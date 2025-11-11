ff9ee27765ad27c376734a931c17d09856f462fe
JielongHe <jielonghe9@gmail.com>
2025-11-11 17:24:01 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,7 +1,35 @@
-torchrun --nproc_per_node=2 train.py \
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --gpus=4
+#SBATCH --partition=gpu_h100
+#SBATCH --time=0:10:00
+#SBATCH --mem=84G
+#SBATCH --exclusive
+#SBATCH --job-name=andes
+#SBATCH -o ./log/andes_%j.out
+
+# =============== 生成时间戳 ===============
+TIMESTAMP=$(date +"%Y%m%d_%H%M")
+LOG_DIR="./log"
+OUTPUT_DIR="./check_qwen3/checkpoints_${TIMESTAMP}"
+
+
+
+# =============== 加载 Snellius 2023 工具链 + CUDA ===============
+module load 2023
+module load CUDA/12.4.0
+
+nvidia-smi
+
+
+source /home/khe/miniconda3/bin/activate qwen
+
+torchrun --nproc_per_node=4 train.py \
   --dataset DGM4 \
-  --epochs 20 \
-  --batch-size 2 \
+  --epochs 10 \
+  --batch-size 32 \
   --lr 5e-5 \
   --lr-scheduler cosine \
   --warmup-ratio 0.1 \
